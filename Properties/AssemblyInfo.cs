c48c67acc18e4736c0b504e2a070be547d146ce1
doge15567 <41397214+doge15567@users.noreply.github.com>
2025-03-06 13:35:27 -0600

    Prep for uploat
    Change MelonColor, fix workflow

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using MelonLoader;
 [assembly: AssemblyCopyright("Developed by " + SpeedGlitchFix.Main.Author)]
 [assembly: AssemblyTrademark(SpeedGlitchFix.Main.Company)]
 [assembly: MelonInfo(typeof(SpeedGlitchFix.Main), SpeedGlitchFix.Main.Name, SpeedGlitchFix.Main.Version, SpeedGlitchFix.Main.Author, SpeedGlitchFix.Main.DownloadLink)]
-[assembly: MelonColor(255,255,255,255)]
+[assembly: MelonColor(255, 138, 138, 138)]
 
 // Create and Setup a MelonGame Attribute to mark a Melon as Universal or Compatible with specific Games.
 // If no MelonGame Attribute is found or any of the Values for any MelonGame Attribute on the Melon is null or empty it will be assumed the Melon is Universal.
